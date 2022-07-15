d5ff6cbcb32a3c8638b34a9a28fa6928add32afe
sergdevoneeight <sergdevoneeight@gmail.com>
2022-07-15 17:29:06 +0300

    Deleted commented unsued code

--- model/spec_items_model.py
+++ model/spec_items_model.py
@@ -47,33 +47,3 @@ class SpecItemsModel:
         # Quit driver after getting all details
         self._driver.quit()
         return items
-
-    # def get_spec_items(self, urls: list[str], locators: dict) -> list[SpecItem]:
-    #     items = []
-    #
-    #     for url in urls:
-    #         logger.info(f'Trying to get details for - {url}')
-    #         locator_key = urllib.parse.urlparse(url).netloc
-    #         logger.info(f'Locator key is {locator_key}')
-    #
-    #         if locator_key in locators:
-    #             # Switching to a new tab for each page
-    #             self._driver.switch_to.new_window('tab')
-    #             spec_item_page = ItemWebPage(self._driver, url, locators[locator_key])
-    #             spec_item_page.open()
-    #
-    #             name = spec_item_page.name
-    #             price = str_utils.str_price_to_float(spec_item_page.price)
-    #             image_url = spec_item_page.image_url
-    #
-    #             logger.info(f'Item name: {name}')
-    #             logger.info(f'Item price: {price}')
-    #             logger.info(f'Item image_url: {image_url}')
-    #             items.append(SpecItem(name, price, image_url))
-    #         else:
-    #             logger.warning(f'Locator key - {locator_key} is not present in specified locators')
-    #             items.append(SpecItem('NOT FOUND', 'NOT FOUND', 'NOT FOUND'))
-    #
-    #     # Quit driver after getting all details
-    #     self._driver.quit()
-    #     return items
